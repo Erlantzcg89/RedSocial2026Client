50f59a49c9906bd92e87e29b55e55cad9c8b6475
Erlantz Caballero García <erlantzcg89@gmail.com>
2025-10-13 15:15:40 +0200

    estilos login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuPrincipalComponent],
+  imports: [RouterOutlet, MenuPrincipalComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MiPerfilComponent } from './components/mi-perfil/mi-perfil.component';
+import { ForoComponent } from './components/foro/foro.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'mi-perfil', component: MiPerfilComponent },
+  { path: 'foro', component: ForoComponent },
   { path: '**', redirectTo: '' }  // redirige rutas no existentes al home
 ];
