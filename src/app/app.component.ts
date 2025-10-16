3be1e24e4451716b5984c94e7f0fe4cc892c6815
Erlantz Caballero García <erlantzcg89@gmail.com>
2025-10-16 11:04:30 +0200

    separando el banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
 import { LoginComponent } from './components/login/login.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuPrincipalComponent, LoginComponent],
+  imports: [RouterOutlet, BannerComponent, MenuPrincipalComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
